6fe1368db28951228aee8727e4164bb72507084a
augustius cokroe <augustius@ebizu.com>
2017-09-09 16:37:10 +0800

    Initial commit

---
+++ AskSean/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AskSean
+//
+//  Created by augustius cokroe on 09/09/2017.
+//  Copyright © 2017 augustius cokroe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
